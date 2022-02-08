96ae4d09ef7fdf45c01234030e3c213343f040b9
flowersmiling <lcl8885@gmail.com>
2022-02-07 17:01:46 -0700

    add selectionsort algorithm

--- src/sait/sort/utilities/Allsorts.java
+++ src/sait/sort/utilities/Allsorts.java
@@ -1,25 +1,41 @@
 package sait.sort.utilities;
 
+import java.util.Comparator;
+
 public class Allsorts 
 {
-	public static void SelectionSort(double arr[])
-    {
-        int n = arr.length;
-  
-        // One by one move boundary of unsorted subarray
-        for (int i = 0; i < n-1; i++)
-        {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
-  
-            // Swap the found minimum element with the first
-            // element
-            double temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
-        }
-    }
+	public static <T extends Comparable<? super T>> void selectionSort(T[] a, int n, Comparator<? super T> comp)
+	{
+		for (int index = 0; index < n-1; index++)
+		{
+			int indexOfNextSmallest = getIndexOfSmallest(a, index, n-1, comp);
+			swap(a, index, indexOfNextSmallest);
+			// Assertion: a[0] <= a[1] <= . . . <= a[index] <= all other a[i].
+		}
+	}
+	
+	private static <T extends Comparable<? super T>> int getIndexOfSmallest(T[] a, int first, int last, Comparator<? super T> comp)
+	{
+		T min = a[first];
+		int indexOfMin = first;
+		for (int index = first + 1; index <= last; index++)
+		{
+			if (comp.compare(a[index], min) < 0)
+			{
+				min = a[index];
+				indexOfMin = index;
+			}
+		// Assertion: min is the smallest of a[first] through a[index].
+		}
+		
+		return indexOfMin;
+	}
+	
+	// Swaps the array entries a[i] and a[j].
+	 private static void swap(Object[] a, int i, int j)
+	 {
+		 Object temp = a[i];
+		 a[i] = a[j];
+		 a[j] = temp;
+	 }
 }
