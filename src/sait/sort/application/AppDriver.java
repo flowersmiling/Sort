e2228d54a2c62e315aecb90f75f6897d7a12abeb
flowersmiling <lcl8885@gmail.com>
2022-02-08 00:11:02 -0700

    add main method

--- src/sait/sort/application/AppDriver.java
+++ src/sait/sort/application/AppDriver.java
@@ -1,9 +1,38 @@
 package sait.sort.application;
 
+import java.util.Scanner;
+
 public class AppDriver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		String strComd = scan.nextLine();
+		
+		if( strComd != "" && strComd != null ) 
+		{
+			String[] cmds = strComd.replaceAll(" \s+", " ").split(" ");
+			
+			for(int i = 0; i < cmds.length; i++) 
+			{
+				if(Character.toLowerCase(cmds[i].charAt(1))=='t') 
+				{
+					System.out.println(cmds[i].charAt(2));
+				}
+				if(Character.toLowerCase(cmds[i].charAt(1))=='s')
+				{
+					System.out.println(cmds[i].charAt(2));
+				}
+				if(Character.toLowerCase(cmds[i].charAt(1))=='f')
+				{
+					String file = cmds[i].toString().replace("\"", "").substring(2);
+					System.out.println(file);
+				}
+				//System.out.println(cmds[i].toString());
+			}
+		}
+		
+		scan.close();
 
 	}
 
